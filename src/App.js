5776b25e26f9cb721ed746ed828fc8a98d24c768
Jeisson Andrés Rubio <jeirub@gmail.com>
2023-11-15 17:07:47 -0500

    se sube el proyecto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [task, setTask] = useState([]);
+  const [newTask, setNewTask] = useState ('');
+
+  const addNewTask = () => {
+    let _task = task
+    _task.push(newTask);
+    setTask(_task);
+    setNewTask('');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-task">
+       <h2>Task list</h2>
+      <div className='container-task-add'>
+        <input type='text' value={newTask} onChange={(e) => setNewTask(e.target.value)}></input>
+        <button onClick={()=> addNewTask () } className='button'>+</button>
+
+      </div>
+     
+      <div className='container-task-list'>
+        
+        {
+          task.map(_task => {
+            return (
+              <h5 key={_task}>{_task}</h5>
+              
+            )
+            
+          })
+        }
+        
+
+      </div>
     </div>
   );
 }
